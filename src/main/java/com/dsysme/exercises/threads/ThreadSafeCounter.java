9c1ab4e43a399b77f04bd1306ca9084022d4683b
Sharon Shmorak <sharon.shmorak@gmail.com>
2018-08-11 09:27:51 +0300

    some fixes to ThreadSafeCounter

--- src/main/java/com/dsysme/exercises/threads/ThreadSafeCounter.java
+++ src/main/java/com/dsysme/exercises/threads/ThreadSafeCounter.java
@@ -2,15 +2,18 @@ package com.dsysme.exercises.threads;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadSafeCounter implements Runnable {
 
+    public static final int MAX_PER_COUNTER = 25000000;
+    public static final int N_THREADS = 4;
     private AtomicInteger atomicCounter = new AtomicInteger();
 
     @Override
     public void run() {
-        for (int i = 0; i < 25000000; ++i) {
+        for (int i = 0; i < MAX_PER_COUNTER; ++i) {
             atomicCounter.incrementAndGet();
         }
     }
@@ -20,15 +23,20 @@ public class ThreadSafeCounter implements Runnable {
     }
 
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        ExecutorService executorService = Executors.newFixedThreadPool(N_THREADS);
         ThreadSafeCounter counter = new ThreadSafeCounter();
 
-        for (int i = 0; i < 4; ++i) {
+        for (int i = 0; i < N_THREADS; ++i) {
             executorService.submit(counter);
         }
 
-        while(counter.getCurrentCount() < 25000000 * 4) {
-            continue;
+        // https://stackoverflow.com/questions/1250643/how-to-wait-for-all-threads-to-finish-using-executorservice
+        executorService.shutdown();
+
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            // What should I do hear?
         }
 
         System.out.println("Current count:" + counter.getCurrentCount());
