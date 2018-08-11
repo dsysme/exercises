0b575f8fd6c17ebc1eecb49cf2cb0ff1549688c7
Sharon Shmorak <sharon.shmorak@gmail.com>
2018-08-11 08:49:24 +0300

    implemented ThreadSafeCounter

--- src/main/java/com/dsysme/exercises/threads/ThreadSafeCounter.java
+++ src/main/java/com/dsysme/exercises/threads/ThreadSafeCounter.java
@@ -1,4 +1,38 @@
 package com.dsysme.exercises.threads;
 
-public class ThreadSafeCounter {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ThreadSafeCounter implements Runnable {
+
+    private AtomicInteger atomicCounter = new AtomicInteger();
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 25000000; ++i) {
+            atomicCounter.incrementAndGet();
+        }
+    }
+
+    public int getCurrentCount() {
+        return atomicCounter.intValue();
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        ThreadSafeCounter counter = new ThreadSafeCounter();
+
+        for (int i = 0; i < 4; ++i) {
+            executorService.submit(counter);
+        }
+
+        while(counter.getCurrentCount() < 25000000 * 4) {
+            continue;
+        }
+
+        System.out.println("Current count:" + counter.getCurrentCount());
+        executorService.shutdown();
+    }
+
 }
